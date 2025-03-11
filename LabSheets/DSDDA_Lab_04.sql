d1d5f079270b61eaabf87f7f49292b0bfb4b951a
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2025-03-11 13:01:12 +0530

    Update DSDDA_Lab_04.sql

--- LabSheets/DSDDA_Lab_04.sql
+++ LabSheets/DSDDA_Lab_04.sql
@@ -1,131 +1,104 @@
--- 1. Create Tables
-CREATE TABLE Department (
-    DeptNo VARCHAR2(20) PRIMARY KEY,
-    DeptName CHAR(20),
-    Location VARCHAR2(20)
-)
+-- Exercise 01
+-- Write a PL/SQL to get the current price of a stock belongs to company ‘IBM’. Store the company name (‘IBM’) in a variable.
+DECLARE
+    var_copany_name VARCHAR2(30) := 'IBM';
+    var_stock_price NUMBER;
+BEGIN
+    SELECT c.stockPrice INTO var_stock_price
+    FROM Company c
+    WHERE c.cname = var_copany_name;
+
+    DBMS_OUTPUT.PUT_LINE('Name of the company is: ' || var_copany_name);
+END;
 /
 
-CREATE TABLE Employee (
-    EmpNo VARCHAR2(20) PRIMARY KEY,
-    fname CHAR(20),
-    lname CHAR(20),
-    address VARCHAR2(40),
-    salary NUMBER,
-    DeptNo VARCHAR2(20),
-    CONSTRAINT fk_DeptNo FOREIGN KEY (DeptNo) REFERENCES Department(DeptNo)
-)
+-- Exercise 02
+-- Write a PL/SQL to display a message for a given stock of ‘IBM’ company according to the following criteria.
+--     Current price < 45 - ‘Current price is very low !’
+--     45<=Current price < 55 -  price is low !’
+--     55<=Current price < 65 -  price is medium !’
+--     65<=Current price < 75 -  price is medium high !’
+--     75<=Current price - ‘Current price is high !’
+-- Store the company name in a variable.
+DECLARE
+    var_copany_name VARCHAR2(30) := 'IBM';
+    var_current_price NUMBER;
+    var_statement VARCHAR2(100);
+BEGIN
+    SELECT c.stockPrice INTO var_current_price
+    FROM Company c
+    WHERE c.cname = var_copany_name;
+
+    IF var_current_price < 45 THEN
+        var_statement := 'Current price is very low !';
+    ELSIF var_current_price < 55 THEN
+        var_statement := 'Price is low !';
+    ELSIF var_current_price < 65 THEN
+        var_statement := 'Price is medium !';
+    ELSIF var_current_price < 75 THEN
+        var_statement := 'Price is medium high !';
+    ELSIF var_current_price >= 75 THEN
+        var_statement := 'Current price is high !';
+    ELSE
+        var_statement := 'Invalid price';
+    END IF;
+
+    DBMS_OUTPUT.PUT_LINE(var_statement);
+END;
 /
 
-CREATE TABLE Project (
-    ProjNo CHAR(5) PRIMARY KEY,
-    Project_Name VARCHAR2(20),
-    DeptNo VARCHAR2(20),
-    CONSTRAINT fk_ProjectDept FOREIGN KEY (DeptNo) REFERENCES Department(DeptNo)
-)
+-- Exercise 03 
+-- Write three PL/SQLs to draw the following shape. Use all three types of loops in each PL/SQL. 
+    -- 9 9 9 9 9 9 9 9 9 
+    -- 8 8 8 8 8 8 8 8 
+    -- 7 7 7 7 7 7 7 
+    -- 6 6 6 6 6 6 
+    -- 5 5 5 5 5 
+    -- 4 4 4 4 
+    -- 3 3 3 
+    -- 2 2 
+    -- 1 
+BEGIN
+    <<i_loop>> FOR i in REVERSE 1..9 LOOP
+        <<j_loop>> FOR j in 1..9 LOOP
+            DBMS_OUTPUT.PUT(i || ' ');
+            EXIT j_loop WHEN j = i;
+        END LOOP;
+        DBMS_OUTPUT.NEW_LINE;
+    END LOOP;
+END;
 /
 
-CREATE TABLE Works_On (
-    EmpNo VARCHAR2(20),
-    ProjNo CHAR(5),
-    DateWorked DATE,
-    Hours NUMBER(2),
-    PRIMARY KEY (EmpNo, ProjNo, DateWorked),
-    CONSTRAINT fk_WorkEmp FOREIGN KEY (EmpNo) REFERENCES Employee(EmpNo),
-    CONSTRAINT fk_WorkProj FOREIGN KEY (ProjNo) REFERENCES Project(ProjNo)
-)
+DECLARE
+    i NUMBER := 9;
+    j NUMBER;
+BEGIN
+    WHILE i >= 1  LOOP
+        j := 1;
+        WHILE j <= i LOOP
+            DBMS_OUTPUT.PUT(i || ' ');
+            j := j + 1;
+        END LOOP;
+        DBMS_OUTPUT.NEW_LINE;
+        i := i - 1;
+    END LOOP;
+END;
 /
 
--- 2. Insert Data
-INSERT INTO Department VALUES
-('001', 'Accounts', 'Bangalore')
-/
-INSERT INTO Department VALUES
-('002', 'IT', 'Mumbai')
-/
-INSERT INTO Department VALUES
-('003', 'ECE', 'Mumbai')
-/
-INSERT INTO Department VALUES
-('004', 'ISE', 'Mumbai')
-/
-INSERT INTO Department VALUES
-('005', 'CSE', 'Delhi')
-/
-
-INSERT INTO Employee VALUES
-('Emp01', 'John', 'Scott', 'Mysore', 45000, '003')
-/
-INSERT INTO Employee VALUES
-('Emp02', 'James', 'Smith', 'Bangalore', 50000, '005')
-/
-INSERT INTO Employee VALUES
-('Emp03', 'Edward', 'Hedge', 'Bangalore', 65000, '002')
-/
-INSERT INTO Employee VALUES
-('Emp04', 'Santhosh', 'Kumar', 'Delhi', 80000, '002')
-/
-INSERT INTO Employee VALUES
-('Emp05', 'Veena', 'M', 'Mumbai', 45000, '004')
-/
-
-INSERT INTO Project VALUES
-('P01', 'IOT', '005')
-/
-INSERT INTO Project VALUES
-('P02', 'Cloud', '005')
-/
-INSERT INTO Project VALUES
-('P03', 'BankMgmt', '004')
-/
-INSERT INTO Project VALUES
-('P04', 'Sensors', '003')
-/
-INSERT INTO Project VALUES
-('P05', 'BigData', '002')
-/
-
-INSERT INTO Works_On VALUES
-('Emp02', 'P03', TO_DATE('2018-10-02', 'YYYY-MM-DD'), 4)
-/
-INSERT INTO Works_On VALUES
-('Emp01', 'P02', TO_DATE('2014-01-22', 'YYYY-MM-DD'), 13)
-/
-INSERT INTO Works_On VALUES
-('Emp02', 'P02', TO_DATE('2020-06-19', 'YYYY-MM-DD'), 15)
-/
-INSERT INTO Works_On VALUES
-('Emp02', 'P01', TO_DATE('2020-06-11', 'YYYY-MM-DD'), 10)
-/
-INSERT INTO Works_On VALUES
-('Emp01', 'P04', TO_DATE('2009-02-08', 'YYYY-MM-DD'), 6)
-/
-INSERT INTO Works_On VALUES
-('Emp02', 'P01', TO_DATE('2018-10-18', 'YYYY-MM-DD'), 18)
-/
-INSERT INTO Works_On VALUES
-('Emp01', 'P05', TO_DATE('2011-09-02', 'YYYY-MM-DD'), 7)
-/
-
-
-
-/*
-* Drop tables
-*/
-ALTER TABLE Works_On DROP CONSTRAINT fk_WorkEmp
-/
-ALTER TABLE Works_On DROP CONSTRAINT fk_WorkProj
-/
-ALTER TABLE Project DROP CONSTRAINT fk_ProjectDept
-/
-ALTER TABLE Employee DROP CONSTRAINT fk_DeptNo
-/
-
-DROP TABLE Works_On 
-/
-DROP TABLE Project 
-/
-DROP TABLE Employee 
-/
-DROP TABLE Department 
+DECLARE
+    i NUMBER := 9;
+    j NUMBER;
+BEGIN
+    LOOP
+        j := 1;
+        LOOP
+            DBMS_OUTPUT.PUT(i || ' ');
+            j := j + 1;
+            EXIT WHEN j > i;
+        END LOOP;
+        DBMS_OUTPUT.NEW_LINE;
+        i := i - 1;
+        EXIT WHEN i = 0;
+    END LOOP;
+END;
 /
